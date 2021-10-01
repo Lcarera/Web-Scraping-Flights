81bea8d8ae4d97abbbc2bf2d59b5dff097dc0585
Lcarera <lcarera@alumno.huergo.edu.ar>
2021-10-01 11:30:24 -0300

    Agrego script para subir 'flight_data' la qnap

---
+++ ftpupload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+mv flights_data.txt flights_data
+
+ftp_user="qt1"
+ftp_pass='YpMePUXK'
+ftp_ip="networkbroadcastqnap.ddns.net"
+ftp_port=21
+ftp_file_to_upload="flights_data"
+ftp_file_destiny="Web/Hotel-Telecable"
+
+ftp -inv <<EOF
+open $ftp_ip $ftp_port
+user $ftp_user $ftp_pass
+cd $ftp_file_destiny
+mdelete $ftp_file_to_upload
+mput $ftp_file_to_upload
+close 
+bye
+EOF
+
